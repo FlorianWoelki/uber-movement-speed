4fd26860d46c4dd0b8e84c6a7f76275ef0594558
FlorianWoelki <FlorianWoelki@gmx.de>
2023-05-26 20:12:10 +0200

    build: extend localstack.sh script to build with docker compose and delete container

--- localstack.sh
+++ localstack.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 source .env
 
-# Tries to start localstack for AWS emulation.
 export LOCALSTACK_API_KEY=${LOCALSTACK_API_KEY}
-localstack start
+
+# Check if `LOCALSTACK_API_KEY`` is set.
+if [ -z "$LOCALSTACK_API_KEY" ]; then
+  echo "LOCALSTACK_API_KEY is not set. Please set it in .env file."
+  exit 1
+fi
+
+# Checks if container exists and removes it.
+if docker ps -a | grep localstack_main; then
+  docker rm -f localstack_main
+fi
+
+# Check if `localstack` is locally installed.
+if ! command -v localstack &> /dev/null
+then
+  # Start localstack with `docker`.
+  docker compose up --build
+else
+  # Start localstack with `localstack` command.
+  localstack start
+fi

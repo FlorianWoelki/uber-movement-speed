73b838b5644d62dccdadadffd741e704b8f016a8
FlorianWoelki <FlorianWoelki@gmx.de>
2023-05-29 10:40:24 +0200

    feat(service): add get_run_logs to glue job CLI

--- services/glue/job.py
+++ services/glue/job.py
@@ -37,10 +37,40 @@ def stop_glue_job(job_name: str, run_id: str):
     )
 
 
+def get_run_logs(job_name: str, run_id: str) -> list[str]:
+    """
+    Gets the logs of a glue job with the given name and run ID.
+
+    Args:
+        job_name (str): The name of the glue job.
+        run_id (str): The ID of the glue job run.
+
+    Returns:
+        list[str]: The logs of the glue job run.
+    """
+    glue = boto3.client("glue", endpoint_url=endpoint_url)
+    runs = glue.get_job_runs(
+        JobName=job_name,
+    )["JobRuns"]
+    run_id = next(run["Id"] for run in runs if run["Id"] == run_id)
+
+    logs = boto3.client("logs", endpoint_url=endpoint_url)
+    response = logs.get_log_events(
+        logGroupName=f"/aws/glue/jobs",
+        logStreamName=run_id,
+    )
+
+    result: list[str] = []
+    for event in response["events"]:
+        result.append(event["message"])
+
+    return result
+
+
 def main():
     job_name = "raw-data-etl"
 
-    # Starts only a glue job.
+    # Starts a glue job.
     if len(sys.argv) == 2 and sys.argv[1] == "start":
         job_run_id = start_glue_job(job_name)
         print(f"Started Glue job run with ID: {job_run_id}")
@@ -58,7 +88,15 @@ def main():
         )
         return
 
-    # Stops only a glue job.
+    # Gets the logs of a glue job.
+    if len(sys.argv) == 3 and sys.argv[1] == "logs":
+        job_run_id = sys.argv[2]
+        logs = get_run_logs(job_name, job_run_id)
+        for log in logs:
+            print(log)
+        return
+
+    # Stops a glue job.
     if len(sys.argv) == 3 and sys.argv[1] == "stop":
         job_run_id = sys.argv[2]
         stop_glue_job(job_name, job_run_id)

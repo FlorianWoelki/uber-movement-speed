f8f3dc503c261bd8ead696ae5acbbe1d7a946369
FlorianWoelki <FlorianWoelki@gmx.de>
2023-06-07 21:18:15 +0200

    feat: add script for checking websocket functionality

---
+++ scripts/test-websocket.py
@@ -0,0 +1,25 @@
+import json
+import boto3
+import asyncio
+import websockets
+
+
+url = "ws://localhost:4510"
+
+
+def main():
+    async def start_client(uri: str):
+        async with websockets.connect(uri) as websocket:
+            print("Sending message to websocket")
+            await websocket.send(
+                json.dumps({"action": "kinesis-data-forwarder", "data": "Hello World"})
+            )
+            result = await websocket.recv()
+            print(f"Received message from websocket: {result}")
+
+    print("Connecting to websocket URL")
+    asyncio.get_event_loop().run_until_complete(start_client(url))
+
+
+if __name__ == "__main__":
+    main()

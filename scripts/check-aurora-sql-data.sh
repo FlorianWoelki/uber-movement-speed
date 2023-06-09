0d8b59aa5392c53f5a4c45f270357e55b6f75ab8
FlorianWoelki <FlorianWoelki@gmx.de>
2023-06-09 20:54:08 +0200

    refactor(script): adjust check-aurora-sql-data script to get all data from table

--- scripts/check-aurora-sql-data.sh
+++ scripts/check-aurora-sql-data.sh
@@ -11,10 +11,10 @@ SECRET=$(aws --endpoint-url=http://localhost:4566 secretsmanager describe-secret
 # Gets the ARN of the secret.
 SECRET_ARN=$(echo $SECRET | jq -r '.ARN')
 
-# Selects all the data in the table `books` in the database.
+# Selects all the data in the table `street_segment_speeds` in the database.
 aws --endpoint-url=http://localhost:4566 rds-data execute-statement \
     --resource-arn $CLUSTER_ARN \
     --secret-arn $SECRET_ARN \
     --database test \
-    --sql "SELECT id, title FROM books" \
+    --sql "SELECT * FROM street_segment_speeds" \
     --output json

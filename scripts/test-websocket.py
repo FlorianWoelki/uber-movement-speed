21b5f608111d5d0ed1c824f0764c65f9dbd133b3
FlorianWoelki <FlorianWoelki@gmx.de>
2023-06-09 20:58:46 +0200

    refactor(script): change data for test-websocket script

--- scripts/test-websocket.py
+++ scripts/test-websocket.py
@@ -14,7 +14,20 @@ def main():
                 json.dumps(
                     {
                         "action": "kinesis-data-forwarder",
-                        "data": {"id": "ws-id", "title": "WS book title"},
+                        "data": {
+                            "year": 2020,
+                            "month": 1,
+                            "day": 1,
+                            "hour": 1,
+                            "utc_timestamp": "2020-01-01T09:00:00.000Z",
+                            "start_junction_id": "8e555723c3dff79036c7a8c0cef6b32a80763c9f",
+                            "end_junction_id": "2278ad9374ec96c35a0d769bc8a275f6355b55da",
+                            "osm_way_id": 40722998,
+                            "osm_start_node_id": 62385707,
+                            "osm_end_node_id": 4927951349,
+                            "speed_mph_mean": 26.636,
+                            "speed_mph_stddev": 4.483,
+                        },
                     }
                 )
             )
